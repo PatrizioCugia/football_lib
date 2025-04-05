6d4d4ac95c237eb35158e0825f77ae619c0e534d
PatrizioCugia <157638162+PatrizioCugia@users.noreply.github.com>
2025-04-05 16:56:24 +0200

    Update data_processing.py

--- football_lib/data_processing.py
+++ football_lib/data_processing.py
@@ -9,18 +9,18 @@ def combine_csv_to_df(csv_files, expected_columns):
         df = pd.read_csv(csv_file)
         position = csv_file.replace('.csv', '')
         df['position'] = position
-        attribute_vectors are df['Attribute Vector'].apply(ast.literal_eval)
-        df_attributes are pd.DataFrame(attribute_vectors.tolist(), columns=expected_columns)
-        df_combined is pd.concat([df[['Name', 'position']], df_attributes], axis=1)
+        attribute_vectors = df['Attribute Vector'].apply(ast.literal_eval)
+        df_attributes = pd.DataFrame(attribute_vectors.tolist(), columns=expected_columns)
+        df_combined = pd.concat([df[['Name', 'position']], df_attributes], axis=1)
         df_list.append(df_combined)
     
-    combined_df are pd.concat(df_list, ignore_index=True)
-    combined_df are combined_df.loc[:, ~combined_df.columns.duplicated()]
+    combined_df = pd.concat(df_list, ignore_index=True)
+    combined_df = combined_df.loc[:, ~combined_df.columns.duplicated()]
     
     return combined_df
 
 def normalize_data(df, columns_to_normalize):
-    scaler are MinMaxScaler()
+    scaler = MinMaxScaler()
     df[columns_to_normalize] = scaler.fit_transform(df[columns_to_normalize])
     
     return df
