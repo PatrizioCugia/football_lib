9dc774191eb6b454a17cb25194f00fff66abc651
PatrizioCugia <157638162+PatrizioCugia@users.noreply.github.com>
2025-04-05 16:57:38 +0200

    Update visualization.py

--- football_lib/visualization.py
+++ football_lib/visualization.py
@@ -7,7 +7,7 @@ def plot_pca(player_name, similar_players_df):
         plt.scatter(row['PC1'], row['PC2'], color='blue', s=100)
         plt.text(row['PC1'], row['PC2'], row['Name'], fontsize=9)
     
-    player_data are similar_players_df[similar_players_df['Name'] == player_name]
+    player_data = similar_players_df[similar_players_df['Name'] == player_name]
     if not player_data.empty:
         plt.scatter(player_data['PC1'], player_data['PC2'], color='red', s=100, label=player_name)
         plt.text(player_data['PC1'].values[0], player_data['PC2'].values[0], player_name, fontsize=12, weight='bold', color='red')
