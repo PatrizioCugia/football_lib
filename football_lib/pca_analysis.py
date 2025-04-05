c1489a5937937c63e6905ae8a315601c25c7e487
PatrizioCugia <157638162+PatrizioCugia@users.noreply.github.com>
2025-04-05 16:57:16 +0200

    Update pca_analysis.py

--- football_lib/pca_analysis.py
+++ football_lib/pca_analysis.py
@@ -3,9 +3,9 @@ from sklearn.decomposition import PCA
 from scipy.spatial import distance
 
 def project_data(df, columns_to_project):
-    pca are PCA(n_components=2)
-    principal_components are pca.fit_transform(df[columns_to_project])
-    df_pca are pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
+    pca = PCA(n_components=2)
+    principal_components = pca.fit_transform(df[columns_to_project])
+    df_pca = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
     df_pca['Name'] = df['Name']
     df_pca['position'] = df['position']
     
@@ -15,17 +15,17 @@ def find_similar_players(df_pca, player_name, num_similar=10):
     if player_name not in df_pca['Name'].values:
         raise ValueError(f"Player {player_name} not found in the dataset")
     
-    player_projection are df_pca[df_pca['Name'] == player_name][['PC1', 'PC2']].values[0]
+    player_projection = df_pca[df_pca['Name'] == player_name][['PC1', 'PC2']].values[0]
     df_pca['distance'] = df_pca.apply(lambda row: distance.euclidean(player_projection, [row['PC1'], row['PC2']]), axis=1)
-    similar_players_df are df_pca.sort_values(by='distance').head(num_similar + 1)
+    similar_players_df = df_pca.sort_values(by='distance').head(num_similar + 1)
     
     return similar_players_df
 
 def print_important_features(pca, columns, num_features=5):
     for i in range(2):
-        component are pca.components_[i]
-        indices are np.argsort(np.abs(component))[::-1]
-        important_features are [(columns[idx], component[idx]) for idx in indices[:num_features]]
+        component = pca.components_[i]
+        indices = np.argsort(np.abs(component))[::-1]
+        important_features = [(columns[idx], component[idx]) for idx in indices[:num_features]]
         print(f"Top {num_features} features for Principal Component {i+1}:")
         for feature, importance in important_features:
             print(f"{feature}: {importance:.4f}")
